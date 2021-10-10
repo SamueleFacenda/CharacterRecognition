032a6f95251132b4c0266a2a28a3fe93c3fd9e64
nene127 <samuele.facenda@gmail.com>
2021-10-10 12:11:01 +0200

    creazione area di disegno

--- src/main/java/pack/characterrecognition/Controller.java
+++ src/main/java/pack/characterrecognition/Controller.java
@@ -1,14 +1,31 @@
 package pack.characterrecognition;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
-
+    private Canvas canva;
+    private GraphicsContext disegno;
+    private ColorPicker cpLine = new ColorPicker(Color.BLACK);
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButtonClick() {
+
+    }
+    public void initialize(){
+        disegno= canva.getGraphicsContext2D();
+        canva.setOnMousePressed(e->{
+            disegno.setStroke(cpLine.getValue());
+            disegno.beginPath();
+            disegno.lineTo(e.getX(), e.getY());
+        });
+        canva.setOnMouseDragged(e->{
+            disegno.lineTo(e.getX(), e.getY());
+            disegno.stroke();
+        });
     }
 }
