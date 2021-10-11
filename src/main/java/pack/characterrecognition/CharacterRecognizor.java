bda204e227d2d0fc66f27aa73821ed072c422d7e
nene127 <samuele.facenda@gmail.com>
2021-10-11 16:15:02 +0200

    inizio progettazione

--- src/main/java/pack/characterrecognition/CharacterRecognizor.java
+++ src/main/java/pack/characterrecognition/CharacterRecognizor.java
@@ -4,6 +4,7 @@ import javafx.scene.image.WritableImage;
 
 public class CharacterRecognizor {
     private WritableImage img;
+    private boolean[][] grid;
     private char carattere;
     public CharacterRecognizor(WritableImage img){
         this.img=img;
